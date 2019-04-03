b67671ab5662e5790be107312db60c4b0db0402e
unknown <kcmp711@gmail.com>
2019-04-02 22:04:04 -0400

    created components directory and FriendInfoForm.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        APP
       </div>
     );
   }
---
+++ friends/src/components/FriendInfoForm.js
@@ -0,0 +1,25 @@
+import React from 'react'; //
+import styled from 'styled-components';
+
+const FriendInputForm = styled.div`
+display: flex;
+flex-direction:column;
+align-items: center;
+justify-content:space-around;
+width: 100%;
+`
+
+ function FriendInfoForm (props) {
+    return (
+        <FriendInputForm className="inputForm">
+            <input type="text" placeholder="Enter Friend's Name" name= "name" />
+            <br/>
+            <input type="text" placeholder="Enter Friend's Age" name= "age" />
+            <br/>
+            <input type="text" placeholder="Enter Friend's Email" name= "email" />
+
+         </FriendInputForm>
+        )
+    }
+
+ export default FriendInfoForm;
