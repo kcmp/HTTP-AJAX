98062601e553e9355ece0bf062fad526981a29e0
unknown <kcmp711@gmail.com>
2019-04-02 22:53:30 -0400

    gave state to App, used axios in App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,13 +3,58 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-import FriendInfoForm from './components/FriendInfoForm'
+import FriendInfoForm from './components/FriendInfoForm';
+import FriendsList from './components/FriendsList';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     friends: [],
+     name: '',
+     age: '',
+     email: '',
+      }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/friends`)
+        .then(res => this.setState({friends: res.data}))
+  }
+
+  handleFriendFormInput = event => {
+    const name = event.target.name;
+    this.setState({ [name]: event.target.value });  
+  }
+
+  handleSubmitBtn = event => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/friends', {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email,
+    })
+      .then(res => {
+        this.setState({ friends: res.data})
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        APP
+
+        <FriendsList friends= {this.state.friends} />
+        <FriendInfoForm handleFriendFormInput = {this.handleFriendFormInput} />
+        <br/>
+        
+        
       </div>
     );
   }
